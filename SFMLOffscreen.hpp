963f8944a6546aec1f10da9f4e48f0b171125541
Arthapz <arthur.laurent4@gmail.com>
2018-08-18 17:56:42 +0200

    Removing useless includes

--- SFMLOffscreen.hpp
+++ SFMLOffscreen.hpp
@@ -1,18 +1,14 @@
 #pragma once
 
-#include <QQuickImageProvider>
-#include <QImage>
-#include <QQuickWindow>
 #include <SFML/Graphics/RenderTexture.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
 
-class SFMLOffscreen : public QObject {
-		Q_OBJECT
+class SFMLOffscreen {
 	public:
 		explicit SFMLOffscreen();
 		~SFMLOffscreen();
 
-		Q_INVOKABLE sf::Image renderFrame();
+		sf::Image renderFrame();
 	private:
 		sf::RenderTexture  m_render_texture;
 		sf::RectangleShape m_shape;
